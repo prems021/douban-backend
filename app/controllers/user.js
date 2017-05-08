f6df1dbf9b09e2c17e26499f13a7d3e048c49688
prems021 <premji.netcom@gmail.com>
2017-05-08 13:20:39 -0700

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,8 +2,17 @@
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('postgres://hnsflljkdhseyr:2b589c9e4a49c30f332004626be8846e241cd8c67d779fa2da179d20062bed87@ec2-54-235-119-27.compute-1.amazonaws.com:5432/d51q8k5ef5phcs');
- 
-
+ var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
 // var User = sequelize.define('tas_users', {
 //     ID: {
 //       type: DataTypes.INTEGER(4),
@@ -52,13 +61,19 @@ module.exports = function (app) {
    sequelize
   .authenticate()
   .then(function(err) {
-   res.send(err);
+  res.send({login:" db exist"});
   })
   .catch(function (err) {
    res.send({login:"user not exist"});
   });
 
-   
+   User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
    
    
 //  User.sync({force: true}).then(function () {
