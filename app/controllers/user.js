10309aa8ea7684dd264e45922ebf3af35b9b91f9
prems021 <premji.netcom@gmail.com>
2017-05-08 11:55:26 -0700

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,13 +1,68 @@
+
 var express = require('express')
 var router = express.Router()
-var Sequelize = require('Sequelize');
+
+var validator = require('validator')
+var jwt = require('jsonwebtoken')
 
 module.exports = function (app) {
   app.use('/user', router)
 }
 
+// Create user
+router.post('/', function (req, res, next) {
+  var email = req.body.email
+  var pass = req.body.pass
+  var name = req.body.name
 
+  // Validator
+  if (!validator.isEmpty(email) && !validator.isEmpty(name) &&
+      !validator.isEmpty(pass) && validator.isEmail(email) &&
+      validator.isLength(name, {min: 2, max: 10})) {
+    // Generate token
+    var token = jwt.sign({
+      name: name,
+      email: email,
+      exp: Math.floor(Date.now() / 1000) + 600
+    }, 'shxhxhxhx')
 
+    res.status(200).send({
+      name: name,
+      email: email,
+      token: token
+    })
+  } else {
+    // Error handle
+    res.status(400).send({
+      error: 'Missing field'
+    })
+  }
+})
+
+// Get user
+router.get('/:id', function (req, res, next) {
+  // var id = req.params.id
+  var token = req.get('Authorization').split(' ')[1]
+
+  // Verify
+  jwt.verify(token, 'shxhxhxhx', function (err, decoded) {
+    // Error handle
+    if (err) {
+      // If the user is legitimate, but token fails
+      // you can re generate token
+      res.status(401).send({
+        error: 'Invalid token'
+      })
+    } else {
+      // Authorization sucess return token
+      res.status(200).send({
+        name: decoded.name,
+        email: decoded.email,
+        token: token
+      })
+    }
+  })
+})
 // router.get('/somple', function (req, res, next)  {
   
 
@@ -101,58 +156,3 @@ module.exports = function (app) {
 // });
 
 // Create user
-router.post('/', function (req, res, next) {
-  var email = req.body.email
-  var pass = req.body.pass
-  var name = req.body.name
-
-  // Validator
-  if (!validator.isEmpty(email) && !validator.isEmpty(name) &&
-      !validator.isEmpty(pass) && validator.isEmail(email) &&
-      validator.isLength(name, {min: 2, max: 10})) {
-    // Generate token
-    var token = jwt.sign({
-      name: name,
-      email: email,
-      exp: Math.floor(Date.now() / 1000) + 600
-    }, 'shxhxhxhx')
-
-    res.status(200).send({
-      name: name,
-      email: email,
-      token: token
-    })
-  } else {
-    // Error handle
-    res.status(400).send({
-      error: 'Missing field'
-    })
-  }
-})
-
-
-
-// Get user
-router.get('/:id', function (req, res, next) {
-  // var id = req.params.id
-  var token = req.get('Authorization').split(' ')[1]
-
-  // Verify
-  jwt.verify(token, 'shxhxhxhx', function (err, decoded) {
-    // Error handle
-    if (err) {
-      // If the user is legitimate, but token fails
-      // you can re generate token
-      res.status(401).send({
-        error: 'Invalid token'
-      })
-    } else {
-      // Authorization sucess return token
-      res.status(200).send({
-        name: decoded.name,
-        email: decoded.email,
-        token: token
-      })
-    }
-  })
-})
