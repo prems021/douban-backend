4465d4dd41c8c03e40e7c912b3e7e2ac940c0d2c
prems021 <premji.netcom@gmail.com>
2017-05-11 20:27:58 -0700

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,17 +2,7 @@
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('postgres://hnsflljkdhseyr:2b589c9e4a49c30f332004626be8846e241cd8c67d779fa2da179d20062bed87@ec2-54-235-119-27.compute-1.amazonaws.com:5432/d51q8k5ef5phcs');
- var User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
+
 // var User = sequelize.define('tas_users', {
 //     ID: {
 //       type: DataTypes.INTEGER(4),
@@ -58,6 +48,17 @@ module.exports = function (app) {
 
  router.get('/test', function (req, res, next)  {
    
+   var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
    
   User.findAll({
   attributes: ['firstname', 'lastname']
